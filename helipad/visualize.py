7d315d3303b1d4650171f794898b7a1d25d3f541
charwick <1117120+charwick@users.noreply.github.com>
2021-04-10 19:02:10 -0400

    Fix event loop error on MPL 3.4

--- helipad/visualize.py
+++ helipad/visualize.py
@@ -209,7 +209,7 @@ class TimeSeries(MPLVisualization):
 		
 		if self.resolution > self.model.param('refresh'): self.model.param('refresh', self.resolution)
 		if self.fig.stale: self.fig.canvas.draw_idle()
-		self.fig.canvas.start_event_loop(0.001) #Listen for user input
+		self.fig.canvas.flush_events() #Listen for user input
 	
 	def toggleLine(self, event):
 		c1 = event.artist					#The label or line that was clicked
@@ -337,7 +337,7 @@ class Charts(MPLVisualization):
 		
 		self.fig.patch.set_facecolor(self.events[t] if t in self.events else 'white')
 		if self.fig.stale: self.fig.canvas.draw_idle()
-		self.fig.canvas.start_event_loop(0.001) #Listen for user input
+		self.fig.canvas.flush_events() #Listen for user input
 	
 	#Update the graph to a particular model time
 	def scrub(self, t):
